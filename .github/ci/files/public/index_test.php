3326732c47c9bbf80077d73bd1e0dc7de5d04b19
Stefan Hagspiel <shagspiel@dachcom.ch>
2025-12-11 22:22:25 +0100

    adjust test setup

--- .github/ci/files/public/index_test.php
+++ .github/ci/files/public/index_test.php
@@ -1,42 +1,28 @@
 <?php
+declare(strict_types=1);
+
 /**
- * Pimcore
+ * OpenDXP
  *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Enterprise License (PEL)
+ * This source file is licensed under the GNU General Public License version 3 (GPLv3).
+ *
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ * @copyright  Copyright (c) Pimcore GmbH (https://pimcore.com)
+ * @copyright  Modification Copyright (c) OpenDXP (https://www.opendxp.ch)
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html  GNU General Public License version 3 (GPLv3)
  */
 
-use OpenDxp\Tool;
-use Symfony\Component\HttpFoundation\Request;
+use OpenDxp\Bootstrap;
 
-include __DIR__ . "/../vendor/autoload_runtime.php";
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    define('OPENDXP_PROJECT_ROOT', __DIR__ . '/..');
-    define('APP_ENV', 'test');
+Bootstrap::setProjectRoot();
 
-    $request = Request::createFromGlobals();
+return static function () {
 
-// set current request as property on tool as there's no
-// request stack available yet
-    Tool::setCurrentRequest($request);
+    Bootstrap::bootstrap();
 
-    \OpenDxp\Bootstrap::bootstrap();
-    /** @var \Pimcore\Kernel $kernel */
-    $kernel = \OpenDxp\Bootstrap::kernel();
-
-// reset current request - will be read from request stack from now on
-    Tool::setCurrentRequest(null);
-
-    $response = $kernel->handle($request);
-    $response->send();
-
-    $kernel->terminate($request, $response);
-    return $kernel;
+    return Bootstrap::kernel();
 };
